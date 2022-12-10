84084b73819a256dcf44ebb50f5edd46dc4e1767
Gizem Gür <55960390+gurgizem@users.noreply.github.com>
2022-12-10 23:48:13 +0300

    Another Structure Change :)
    I started on a new code from the scratch, and main page finally started get into shape.

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,121 @@
-
-
-
-
-//import '/package:webapp/screens/mainScreen.dart';
-import './screens/userScreen.dart';
-import './screens/programScreen.dart';
-import './screens/profileScreen.dart';
-import './screens/exerciseScreen.dart';
-import './screens/loginScreen.dart';
-import './themes/customTheme.dart';
+// main.dart
 
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => AuthProvider()),
-        ChangeNotifierProvider(create: (context) => WorkoutProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: CustomTheme.Color3,
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: CustomTheme.Color3,
-            backgroundColor: CustomTheme.Color1,
-            accentColor: CustomTheme.Color2,
-            errorColor: CustomTheme.Color5,
-          ),
-          textTheme: const TextTheme(
-            headline4: TextStyle(
-              color: CustomTheme.Color4,
-              fontSize: 30,
-              overflow: TextOverflow.fade,
+    return MaterialApp(
+      title: 'Explore',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    List _isHovering = [false, false, false, false];
+
+    
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: Container(
+          color: Color(0xFF88AB75),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                //logo will be added
+                Expanded(
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ),
+                SizedBox(width: screenSize.width /50),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    'Exercises',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: screenSize.width / 50),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    'Programs',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: screenSize.width / 50),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    'Users',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: screenSize.width / 50),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    'Profile',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  width: screenSize.width / 50,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    'Log Out',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-        home: const LoginScreen(),
-        routes: {
-          LoginScreen.routeName: (ctx) => const LoginScreen(),
-          ExerciseScreen.routeName: (ctx) => const ExerciseScreen(),
-          MainScreen.routeName: (ctx) => const MainScreen(),
-          ProfileScreen.routeName: (ctx) => const ProfileScreen(),
-          ProgramScreen.routeName: (ctx) => const ProgramScreen(),
-          UserScreen.routeName: (ctx) => const UserScreen(),
-        },
       ),
+      body: SizedBox(
+        width: screenSize.width, height: screenSize.height,
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.all(100),
+            child: Column(
+              children: [
+                Center(
+                  child: InkWell(
+                    child: Text(
+                      "Today’s Accomplishments:",
+                      style: TextStyle(fontSize: 50 ,color: Colors.black),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),  
+      )
     );
   }
 }
